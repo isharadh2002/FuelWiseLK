cb4aa836891eb743d79f580243c59ab11621f0f6
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2024-12-13 13:39:37 +0530

    qr_code_scanner screen updated

--- Mobile-app/fuel_quota_app/lib/screens/qr_scanner.dart
+++ Mobile-app/fuel_quota_app/lib/screens/qr_scanner.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
-import 'vehicle_details.dart';
+import 'package:fuel_quota_app/screens/vehicle_details.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class QRScanner extends StatelessWidget {
+class QRScanner extends StatefulWidget {
   const QRScanner({super.key});
+
+  @override
+  _QRScannerState createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController qrController) {
+    controller = qrController;
+    controller?.scannedDataStream.listen((scanData) {
+      final vehicleId = scanData.code;
+
+      if (vehicleId != null) {
+        controller?.pauseCamera();
+
+        // Redirect to VehicleDetails page with the vehicleId
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => VehicleDetails(vehicleId: vehicleId),
+          ),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('QR Scanner'),
+        title: const Text('Scan QR Code'),
         centerTitle: true,
       ),
-      body: MobileScanner(
-        onDetect: (capture) {
-          final List<Barcode> barcodes = capture.barcodes;
-          for (final barcode in barcodes) {
-            final String? data = barcode.rawValue;
-            if (data != null) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => VehicleDetails(vehicleId: data),
-                ),
-              );
-              break;
-            }
-          }
-        },
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
       ),
     );
   }
