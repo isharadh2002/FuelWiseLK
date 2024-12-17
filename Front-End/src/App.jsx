9b4c54033f4fb74098187b12d33b5f59dbabaef6
Ishara Dhanushan <isharadh2002@gmail.com>
2024-12-17 14:50:25 +0530

    Modifying footer component

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import VehicleRegistrationPage from "./pages/VehicleRegistrationPage";
 import Header from "./components/common/Header";
-import Footer from "./components/common/Footer.jsx";
+import Footer from "./components/common/Footer";
 
 function App() {
     return (
@@ -13,9 +13,9 @@ function App() {
 
                     <Route path="/Home" element={<Header/>}/>
                     <Route path="/" element={<VehicleRegistrationPage/>}/>
+                    <Route path='/footer' element={<Footer/>}/>
                 </Routes>
             </Router>
-            <Footer/>
         </>
     );
 }
