a3bcb1d2874945bc955379ac4df37b65373f5f4e
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-13 20:46:08 +0530

    Vehicle DTO added

---
+++ Back-End/src/main/java/com/example/Back_End/DTO/VehicleDTO.java
@@ -0,0 +1,10 @@
+package com.example.Back_End.DTO;
+
+import lombok.Data;
+
+@Data
+public class VehicleDTO {
+    private int vehicleId;
+    private String registrationNumber;
+    private String ownerName;
+}
