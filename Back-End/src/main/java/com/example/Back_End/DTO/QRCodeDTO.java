b6e400e0a47292ddd5488e09987b0d61efd1e832
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-13 20:35:11 +0530

    small fixes

--- Back-End/src/main/java/com/example/Back_End/DTO/QRCodeDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.Back_End.DTO;
-
-import lombok.Data;
-
-@Data
-public class QRCodeDTO {
-    private int vehicleId;
-    private String qrCodeData; // Base64 string of the QR Code
-}
--- Back-End/src/main/java/com/example/Back_End/Services/QRCodeService.java
+++
@@ -1,8 +0,0 @@
-package com.example.Back_End.Services;
-
-import com.example.Back_End.DTO.QRCodeDTO;
-
-public interface QRCodeService {
-    QRCodeDTO generateQRCodeForVehicle(int vehicleId);
-    QRCodeDTO getQRCodeByVehicleId(int vehicleId);
-}
