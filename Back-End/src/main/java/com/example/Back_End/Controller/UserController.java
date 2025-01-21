7718b80e792a16d333d5199bb811b364b668c4da
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-01-21 19:51:35 +0530

    Added an AdminRepository interface and did some minor changes to the code

--- Back-End/src/main/java/com/example/Back_End/Controller/UserController.java
+++ Back-End/src/main/java/com/example/Back_End/Controller/UserController.java
@@ -2,10 +2,8 @@ package com.example.Back_End.Controller;
 
 import com.example.Back_End.DTO.LoginDTO;
 import com.example.Back_End.DTO.UserDTO;
-import com.example.Back_End.DTO.VehicleOwnerDTO;
 import com.example.Back_End.Response.LoginResponse;
 import com.example.Back_End.Services.UserService;
-import com.example.Back_End.Services.VehicleOwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +15,14 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    @PostMapping(path = "/save")
-    public String saveVehicleOwner(@RequestBody UserDTO userDTO)
-
-    {
-        String id = userService.addUser(userDTO);
-        return id;
+    @PostMapping("/save")
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO userDTO) {
+        try {
+            String userId = userService.addUser(userDTO);
+            return ResponseEntity.ok().body("User saved successfully with ID: " + userId);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error saving user: " + e.getMessage());
+        }
     }
 
     @PostMapping(path = "/login")
--- Back-End/src/main/java/com/example/Back_End/DTO/UserDTO.java
+++ Back-End/src/main/java/com/example/Back_End/DTO/UserDTO.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class UserDTO {
 
-    private int userId;
     private String userName;
     private String password;
     private String email;
