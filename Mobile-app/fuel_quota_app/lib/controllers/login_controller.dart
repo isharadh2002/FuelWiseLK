16723f1c423e38d0403e5ba98c484a497e339be8
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-22 13:25:11 +0530

    logout function added to the dashboard

--- Mobile-app/fuel_quota_app/lib/controllers/login_controller.dart
+++ Mobile-app/fuel_quota_app/lib/controllers/login_controller.dart
@@ -1,8 +1,14 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../screens/dashboard.dart';
+
+Future<void> storeUserId(String userId) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('userId', userId);
+}
 
 class LoginController {
   static const String baseUrl = 'http://10.0.2.2:8080/api/v1/VehicleOwner';
@@ -31,6 +37,11 @@ class LoginController {
         final responseBody = jsonDecode(response.body);
 
         if (responseBody['status'] == true) {
+
+          // final String userId = responseBody['userId'];
+          //
+          // await storeUserId(userId);
+
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Login successful')),
           );
