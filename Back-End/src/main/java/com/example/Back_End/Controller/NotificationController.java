dd2103b33ffbd19d8f754cdd1e7fd3a7e485a95c
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-01-24 23:40:15 +0530

    Finished the SMS Service

--- Back-End/src/main/java/com/example/Back_End/Controller/NotificationController.java
+++ Back-End/src/main/java/com/example/Back_End/Controller/NotificationController.java
@@ -1,4 +1,26 @@
 package com.example.Back_End.Controller;
 
+import com.example.Back_End.DTO.NotificationDTO;
+import com.example.Back_End.Services.FuelQuotaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin // Adjust allowed origins or methods as needed
+@RequestMapping("api/v1/User")
 public class NotificationController {
+
+    @Autowired
+    private FuelQuotaService fuelQuotaService;
+
+//    @PostMapping("/update-quota")
+//    public ResponseEntity<String> updateFuelQuota(@RequestBody NotificationDTO notificationDTO) {
+//        try {
+//            String response = fuelQuotaService.updateFuelQuota(notificationDTO.getVehicleId(), notificationDTO.getPumpedLiters());
+//            return ResponseEntity.ok(response);
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
 }
