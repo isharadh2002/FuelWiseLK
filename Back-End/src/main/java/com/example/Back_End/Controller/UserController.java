165f534652a5903ec07faf29861301c383e44340
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-25 21:21:49 +0530

    Commit

--- Back-End/src/main/java/com/example/Back_End/Controller/UserController.java
+++ Back-End/src/main/java/com/example/Back_End/Controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin // Adjust allowed origins or methods as needed
 @RequestMapping("api/v1/User")
 public class UserController {
 
@@ -28,10 +28,32 @@ public class UserController {
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity<?> loginEmployee(@RequestBody LoginDTO loginDTO)
+    public ResponseEntity<?> loginUser(@RequestBody LoginDTO loginDTO)
 
     {
         LoginResponse loginResponse = userService.loginUser(loginDTO);
         return ResponseEntity.ok(loginResponse);
     }
+
+    @PostMapping("/RegMobileUser")
+    public ResponseEntity<?> saveMobileUser(@RequestBody UserDTO userDTO) {
+
+        try {
+            String userId = userService.addMobileUser(userDTO);
+            return ResponseEntity.ok().body("User saved successfully with ID: " + userId);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error saving user: " + e.getMessage());
+        }
+
+    }
+
+    @PostMapping(path = "/loginMobileUser")
+    public ResponseEntity<?> loginMobileUser(@RequestBody LoginDTO loginDTO)
+
+    {
+        LoginResponse loginResponse = userService.loginMobileUser(loginDTO);
+        return ResponseEntity.ok(loginResponse);
+    }
+
+
 }
