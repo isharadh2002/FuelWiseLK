384671c6236310e143c52cdef350d4a0ac81f522
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-27 22:54:18 +0530

    fix: customer dashboard added

---
+++ Front-End/src/components/customer/ManageProfilePage.jsx
@@ -0,0 +1,12 @@
+// ManageProfilePage.jsx
+import React from 'react';
+
+function ManageProfilePage() {
+    return (
+        <div>
+            {/* Your component code here */}
+        </div>
+    );
+}
+
+export default ManageProfilePage;  // Ensure this line is present
