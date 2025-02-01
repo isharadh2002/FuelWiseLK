d80725d794a2826f2cbae465b14cacd1e0e01615
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-02-01 10:13:45 +0530

    customer dashboard header added

---
+++ Front-End/src/components/customer/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <div className="w-full bg-blue-600 text-white p-4 flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Customer Dashboard</h1>
+      </div>
+    );
+  };
+  
+  export default Header;
+  
